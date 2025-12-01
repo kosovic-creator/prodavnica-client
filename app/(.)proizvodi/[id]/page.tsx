7472db481d19b4008c52cbef656fdaf9d915ccf8
kosovic-creator <drasko.kosovic@icloud.com>
2025-12-01 19:22:06 +0100

    api proizvod/id uklonjen intercepter

--- app/(.)proizvodi/[id]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function ProizvodPage() {
-  return <div>Proizvod detalji</div>;
-}
--- app/(.)proizvodi/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function ProizvodiLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
